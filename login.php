cb9b142d61167ee34bb40bc986e680aee2546829
小澤友也 <ozawa@ozawatomoya-no-MacBook-Air.local>
2015-06-30 00:05:48 +0900

    test

--- login.php
+++ login.php
@@ -3,6 +3,11 @@ require_once("vendor/autoload.php");
 // Make sure to load the Facebook SDK for PHP via composer or manually
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookRequestException;
+use Facebook\GraphUser;
+use Facebook\GraphLocation;
+use Facebook\GraphSessionInfo;
 
 session_start();
 
@@ -10,17 +15,17 @@ session_start();
 FacebookSession::setDefaultApplication('452878368210796', 'f0e25cc2d8ce6f0a8e2e80bf35c64081');
 
 $helper = new FacebookRedirectLoginHelper('http://fb-study-ozawa.herokuapp.com/login.php');
-
+print_r($helper);
 try {
  $session = $helper->getSessionFromRedirect();
  // When Facebook returns an error
 } catch( Exception $ex ) {
  // When validation fails or other local issues
 }
-
+print_r($session);
 //セッションを保持している場合は、main.phpに飛ぶ。それ以外(初回)は、facebookへリクエストを送る
 if ( isset( $session ) ) {
-print_r($session);
+
 //セッション情報と、ログアウトURLをmain.phpに渡す
 //$_SESSION['session'] = $session;
 //$_SESSION['logout_url'] = $helper->getLogoutUrl($session, 'http://fb-study-ozawa.herokuapp.com/index.php');
