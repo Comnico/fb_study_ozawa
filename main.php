ae772e90710edc570addbc14ed6dc0f26d03a468
t.ozawa <t.ozawa@comnico.jp>
2015-07-31 18:51:03 +0900

    main.phpに修正

--- main.php
+++ main.php
@@ -101,6 +101,50 @@ foreach ($data as $d) {
 </div>
 
 <div class="footer">
+
+<script>
+    window.intercomSettings = {
+        app_id: "juh7du4f",
+        name: "Ozawas", // Full name
+        email: "afuv846vtew3@sute.jp", // Email address
+        created_at: 1438300800 // Signup date as a Unix timestamp
+    };
+</script>
+<script>
+    (function() {
+        var w = window;
+        var ic = w.Intercom;
+        if (typeof ic === "function") {
+            ic('reattach_activator');
+            ic('update', intercomSettings);
+        } else {
+            var d = document;
+            var i = function() {
+                i.c(arguments)
+            };
+            i.q = [];
+            i.c = function(args) {
+                i.q.push(args)
+            };
+            w.Intercom = i;
+
+            function l() {
+                var s = d.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = 'https://widget.intercom.io/widget/juh7du4f';
+                var x = d.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+            }
+            if (w.attachEvent) {
+                w.attachEvent('onload', l);
+            } else {
+                w.addEventListener('load', l, false);
+            }
+        }
+    })()
+</script>
+
 <text>&copy; 2015 comnico inc.</text>
 </div>
  </body>
